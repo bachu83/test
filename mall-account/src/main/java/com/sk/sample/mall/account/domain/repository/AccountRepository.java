37a37c3b97a624c22559f8933d6d712e53ec02a6
P116561 <P116561@SKTP116561PN001.SKT.AD>
2019-05-15 15:02:12 +0900

    add findMembershipLevelType

--- mall-account/src/main/java/com/sk/sample/mall/account/domain/repository/AccountRepository.java
+++ mall-account/src/main/java/com/sk/sample/mall/account/domain/repository/AccountRepository.java
@@ -8,10 +8,12 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.sk.sample.mall.account.domain.model.Account;
 import com.sk.sample.mall.account.domain.model.MemberType;
+import com.sk.sample.mall.account.domain.model.MembershipLevelType;
 
 @RepositoryRestResource
 public interface AccountRepository extends PagingAndSortingRepository<Account, Long> {
 	Account findByEmail(String email);
 	Account findByNameLike(String name);
 	List <Account> findByMemberType(@Param("memberType") MemberType memberType);
+	List <Account> findByMembershipLevelType(@Param("membershipLevelType") MembershipLevelType membershipLevelType);
 }

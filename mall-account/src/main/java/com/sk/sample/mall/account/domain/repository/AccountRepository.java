ffff31b5f406d46d0e505076736726d276663f58
P116561 <P116561@SKTP116561PN001.SKT.AD>
2019-05-15 14:53:29 +0900

    adds findByMemberType

--- mall-account/src/main/java/com/sk/sample/mall/account/domain/repository/AccountRepository.java
+++ mall-account/src/main/java/com/sk/sample/mall/account/domain/repository/AccountRepository.java
@@ -1,13 +1,17 @@
 package com.sk.sample.mall.account.domain.repository;
 
-import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.sk.sample.mall.account.domain.model.Account;
+import com.sk.sample.mall.account.domain.model.MemberType;
 
 @RepositoryRestResource
 public interface AccountRepository extends PagingAndSortingRepository<Account, Long> {
 	Account findByEmail(String email);
 	Account findByNameLike(String name);
+	List <Account> findByMemberType(@Param("memberType") MemberType memberType);
 }
